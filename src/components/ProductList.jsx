cea834671f50d952fafb38a29d6651bdaa6ee085
Mayank Rawat <rmayank451@gmail.com>
2026-05-27 17:18:33 +0530

    title updated

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -35,7 +35,7 @@ function ProductList() {
 
   return (
     <div>
-      <h1>Products</h1>
+      <h1 className="text-xl mx-5 font-bold">Products</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
         {filteredProducts.map((product) => (
